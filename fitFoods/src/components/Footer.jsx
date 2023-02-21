7dabcc841ce7602b8724f738bc940bc68e3b35f0
Alvaro Farreny <farrenyalvaro@gmail.com>
2023-02-21 13:32:17 +0100

    footer done

--- fitFoods/src/components/Footer.jsx
+++ fitFoods/src/components/Footer.jsx
@@ -3,24 +3,23 @@ import styles from './Footer.module.css';
 const Footer = () => {
 	return (
 		<>
-			<Footer>
-				<div className={styles.wrapper}>
-					<img src='logo.svg' alt='fitfoods' />
-					<div className='social'>
-						<div className='text'>
-							<p>
-								Any Queries? Visit us in <br />
-							</p>
-							<a href='#'>www.fitfoods.com</a>
-						</div>
-						<div className='links'>
-							<ul>
-								<li>f</li>
-							</ul>
-						</div>
+			<div className={styles.wrapper}>
+				<img src='logo.svg' alt='fitfoods' />
+				<div className={styles.social}>
+					<div className={styles.rectangle}></div>
+					<div>
+						<p>
+							Any Queries? Visit us in <br />
+						</p>
+						<a href='#'>www.fitfoods.com</a>
+					</div>
+					<div className={styles.logos}>
+						<img src='instagram.svg' alt='' />
+						<img src='twitter.svg' alt='' />
+						<img src='facebook.svg' alt='' />
 					</div>
 				</div>
-			</Footer>
+			</div>
 		</>
 	);
 };
