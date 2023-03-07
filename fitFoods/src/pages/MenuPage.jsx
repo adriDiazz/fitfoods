5fe291bca1f8d6afd9f0b8a2bdb96d4e364184e1
Alvaro Farreny <farrenyalvaro@gmail.com>
2023-03-07 14:02:21 +0100

    menus empezados

--- fitFoods/src/pages/MenuPage.jsx
+++ fitFoods/src/pages/MenuPage.jsx
@@ -1,5 +1,33 @@
+import style from './MenuPage.module.css';
+import Button from '../components/UI/Button';
+
 const MenuPage = () => {
-	return <div>MenuPage</div>;
+	return (
+		<div>
+			<h2 className={style.titulo}>Put your diet on autopilot</h2>
+			<div className={style.rectangulo}>
+				<h3 className={style.titulocalorias}>Calories you want to eat</h3>
+				<div className={style.input}>
+					{' '}
+					<input
+						className={style.inputcalorias}
+						type='text'
+						name='calories'
+						placeholder='#### Calories'
+					></input>
+				</div>
+				<div className={style.buttondiv}>
+					{' '}
+					<Button>Get your Menu</Button>
+				</div>
+				<p className={style.titulomenu}>Menú plan for today: 3014 calories</p>
+				<div className={style.rectanguloreceta}>
+					<h2 className={style.typemeal}>Breakfast</h2>
+					<span className={style.caloriesmeal}>Calorias</span>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MenuPage;
