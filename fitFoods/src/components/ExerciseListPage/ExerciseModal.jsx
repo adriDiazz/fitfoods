c026f4d6e18039e1ae3870fe3fee745152dea381
Alvaro Farreny <farrenyalvaro@gmail.com>
2023-04-18 13:14:53 +0200

    updated functions in arrays

--- fitFoods/src/components/ExerciseListPage/ExerciseModal.jsx
+++ fitFoods/src/components/ExerciseListPage/ExerciseModal.jsx
@@ -15,13 +15,23 @@ const ExerciseModal = ({ exercise, steps }) => {
 			<div className={style.info}>
 				<h1>{exercise.name}</h1>
 				<span>{exercise.difficulty}</span>
-
 				<ul className={style.list}>
-					{steps.data.map(step => {
+					{steps.data.slice(0, 1).map((step, stepIndex) => {
 						return (
 							<li key={step.id} className={style.step}>
 								<p>{step.name}</p>
-								<p>{step.description}</p>
+								<p>
+									{step.description
+										.split('.')
+										.filter(sentence => sentence.trim() !== '')
+										.map((sentence, index) => {
+											return (
+												<p key={index}>
+													{index + 1}- {sentence.trim()}
+												</p>
+											);
+										})}
+								</p>
 							</li>
 						);
 					})}
